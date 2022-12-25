5fa37f801d08301cf9ce9a355cd744e5f835b586
mamoudou <konemamoudou84@gmail.com>
2022-12-25 18:52:04 +0100

    insertion de logo svg et animation 1 modif

---
+++ src/components/logo/LogoAnime.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img src="LogoBas.svg" alt="logo stroke">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
