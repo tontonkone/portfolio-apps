ae87d8e2af0883a880c49a5b5da24ddd47c98a72
mamoudou <konemamoudou84@gmail.com>
2023-01-12 23:19:12 +0100

    add mail contact

--- src/views/public/Contact.vue
+++ src/views/public/Contact.vue
@@ -1,124 +1,193 @@
 <template>
-    <div class="contact">
+  <div class="contact">
 
-        <div class="container">
-        <div class="container__img">
-            <img src="@/assets/img/message-c.svg" alt="photo de profils">
+    <div class="container">
+      <div class="container__img">
+        <img src="@/assets/img/message-c.svg" alt="photo de profils">
+      </div>
+
+      <div class="container__info">
+        <LetterTitle>
+          <h1>Contact</h1>
+        </LetterTitle>
+        <div class="text-center">
+          <div>
+
+            <input type="text" name="user_name" v-model="name" placeholder="name" class="pinput" :class="{
+              pgray: !nightMode,
+              'pgray-dark': nightMode,
+              'text-light': nightMode,
+            }" style="transition-delay: 0.2s" />
+          </div>
+
+          <div>
+            <input type="email" name="user_email" v-model="email" placeholder="email" class="pinput" :class="{
+              pgray: !nightMode,
+              'pgray-dark': nightMode,
+              'text-light': nightMode,
+            }" style="transition-delay: 0.4s" />
+          </div>
+
+          <div>
+            <textarea name="message" v-model="text" placeholder="message" class="pinput" rows="4" :class="{
+              pgray: !nightMode,
+              'pgray-dark': nightMode,
+              'text-light': nightMode,
+            }" style="transition-delay: 0.6s"></textarea>
+          </div>
+
+          <button @click.prevent="sendEmail">
+            Envoyer
+          </button>
         </div>
 
-            <div class="container__info">
-                <LetterTitle>
-                    <h1>Contact</h1>
-                </LetterTitle>
+        <Snackbar :showSnackbar="showSnackbar" @close="closeSnackbar" :snackbarMessage="snackbarMessage"
+          :snackbarColor="snackbarColor" />
+      </div>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
-                    inventore quod
-                    quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur
-                    assumenda
-                    nam!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
-                    inventore quod
-                    quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur
-                    assumenda
-                    nam!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus
-                    inventore quod
-                    quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur
-                    assumenda
-                    nam!</p>
-            </div>
-        </div>
     </div>
+  </div>
 </template>
 <script>
 import LetterTitle from '@/components/letter/LetterTitle.vue';
-import {string,objet} from 'yup'
-import { object } from 'yup/lib/locale';
+import emailjs from '@emailjs/browser';
+import Snackbar from '@/components/help/Snackbar.vue';
+import config from '@/config.js'
+
 export default {
-    components: { LetterTitle },
-    setup() {
+  name: "Contact",
+  components: {
+    Snackbar,
+  },
+  props: {
+    nightMode: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      email: "",
+      name: "",
+      text: "",
+      showSnackbar: false,
+      snackbarMessage: "",
+      snackbarColor: "",
+    };
+  },
+  methods: {
+    closeSnackbar(val) {
+      if (!val) {
+        setTimeout(() => {
+          this.showSnackbar = val;
+        }, 1000);
+      }
+    },
+    sendEmail() {
+      if (!this.email || !this.name || !this.text) {
+        this.showSnackbar = true;
+        this.snackbarMessage = "Please all the fields";
+        this.snackbarColor = "rgb(212, 149, 97)";
+      } else {
+        var obj = {
+          user_email: this.email,
+          from_name: this.name,
+          message_html: this.text,
+          to_name: "Natain Bogdan",
+        };
 
-        const schema = object().shape({
-            email: string()
-                        .required("Email requis")
-                        .email("Entrez un email valide"),
-            name: string()
-                        .required("Nom requis"),
-            subject: string()
-                        .required("Sujet requis"),
-            message: string()
-                        .required("Message requis"),
-        })
-        const values = ref({
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-        });
-        const errors = ref({
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
-        })
-        const load = false;
+        emailjs
+          .send(
+            config.emailjs.serviceID,
+            config.emailjs.templateID,
+            obj,
+            config.emailjs.userID
+          )
+          .then(
+            (result) => {
+              this.showSnackbar = true;
+              this.snackbarMessage = "Thanks! Message recieved.";
+              this.snackbarColor = "#1aa260";
 
-        const validate = (field)=> {
-            schema.value
-                   .validateAt(field, values.value)
-                   .then(()=>{ 
-                       errors.value[field] = "";
-                   })
-                   .catch((err)=> {
-                    errors.value[field] = err.message;
-                   });
-
-        }
-
-        const sendMail = () => {
-            load.value = true;
-            schema.value
-                .validate(values.value, { abortEarly: false })
-                .then((res) => {
-                    errors.value = {};
-
-                    emailjs.sendForm('SERVICE_ID', 'TEMPLATE_ID', form.value, 'USER_ID')
-                        .then(() => {
-                            alert('Message sent!')
-                            load.value = false;
-                            values.value = {};
-                        }, (error) => {
-                            alert('Message not sent', error);
-                        });
-                })
-        }
-    }
-}
+              this.email = "";
+              this.text = "";
+              this.name = "";
+            },
+            (error) => {
+              this.showSnackbar = true;
+              this.snackbarMessage = "Oops! Something went wrong.";
+              this.snackbarColor = "rgb(212, 149, 97)";
+            }
+          );
+      }
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-.container {
-    display: flex;
-    flex-direction: row;
+<style scoped>
+.title {
+  font-size: 30px;
+  font-weight: 500;
+}
 
-    &__info {
+.title1 {
+  font-size: 24px;
+  font-weight: 400;
+}
 
-        margin-top: 8rem;
+.title2 {
+  font-size: 20px;
+  font-weight: 400;
+}
 
-        p {
-            text-align: left;
-        }
-    }
+.title3 {
+  font-size: 16px;
+  font-weight: 400;
+}
 
-    &__img {
+.pinput {
+  font-size: 18px;
+  outline: none;
+  border: none;
+  border-radius: 7px;
+  padding: 10px;
+  width: 50%;
+  transition: all 1s;
+}
 
-        margin-top: 10rem;
-        margin-left: 10rem;
-        padding: 2rem;
-    }
+.btn {
+  border-color: #669db3ff;
+  color: #669db3ff;
+  width: 50%;
+}
 
-    p {
-        color: yellow;
-    }
+.btn:hover {
+  background-color: #669db3ff;
+  border-color: #669db3ff;
+  color: white;
+}
+
+.btn:focus {
+  background-color: #669db3ff;
+  border-color: #669db3ff;
+  color: white;
+}
+
+.pgray-dark {
+  background-color: #3c4148 !important;
+}
+
+@media screen and (max-width: 1000px) {
+  .pinput {
+    width: 90%;
+  }
+
+  .pinput {
+    width: 90%;
+  }
+
+  .btn {
+    width: 90%;
+  }
 }
 </style>
